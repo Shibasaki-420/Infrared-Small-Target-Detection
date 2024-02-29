898be7beead3d9b928ce7736cda7331a31db995a
Shibasaki-420 <2223973143@qq.com>
2024-02-29 08:16:59 +0800

    [improve2] 改进实验SPDConv

--- model/model_DNANet.py
+++ model/model_DNANet.py
@@ -86,8 +86,8 @@ class Res_CBAM_block(nn.Module):
         out = self.bn2(out)
 
         # TODO: 消融实验1 ca/sa
-        # out = self.ca(out) * out    # 通道注意力
-        # out = self.sa(out) * out    # 空间注意力
+        out = self.ca(out) * out    # 通道注意力
+        out = self.sa(out) * out    # 空间注意力
 
         out += residual             # 残差连接
 
@@ -99,11 +99,19 @@ class SPDConv(nn.Module):
     def __init__(self, channels):
         super().__init__()
         self.channels = channels
-        self.conv = nn.Conv2d(4*self.channels, self.channels, kernel_size=3, padding=1, bias=False)
+        self.bn1   = nn.BatchNorm2d(4*self.channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(4*self.channels, self.channels, kernel_size=3, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(self.channels)
+        self.relu2 = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
-        return self.conv(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        return self.relu2(x)
 
 class DNANet(nn.Module):
     def __init__(self, num_classes, input_channels, block, num_blocks, nb_filter,deep_supervision=False):
